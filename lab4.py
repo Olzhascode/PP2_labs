a2f5ac520413714f82cf56be6cf49b2c735d9c38
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-02-24 16:13:14 +0500

    json

--- lab4.py
+++ lab4.py
@@ -1,30 +1,22 @@
-import math
+import json
 
-# task 1 
+def parse_interface_status(filename="sample-data.json"):
+        
+        with open(filename, 'r') as f:
+            data = json.load(f)
 
-n = int(input('Enter degree: '))
-print('radian equal to ', n * (math.pi/180))
-print()
+        print("Status")
+        print("=" * 85)
+        print("{:<50} {:<20} {:<6} {:<6}".format("DN", "Description", "Speed", "MTU"))
+        print("-" * 85)
 
+        for interface in data["imdata"]:
+            attributes = interface["l1PhysIf"]["attributes"]
+            dn = attributes["dn"]
+            description = attributes.get("descr", "")  
+            speed = attributes.get("speed", "inherit")
+            mtu = attributes.get("mtu", "9150")
 
-# task 2
+            print("{:<50} {:<20} {:<6} {:<6}".format(dn, description, speed, mtu))
 
-h = int(input('Enter height: '))
-a = int(input('Enter first base: '))
-b = int(input('Enter second base: '))
-print('Area equal to: ', (a + b)*(h/2))
-
-
-# task 3
-    
-s = int(input("Enter number of sides: "))
-l = int(input("Enter the length of a polygon: "))
-ar = s * (l ** 2) / (4 * math.tan(math.pi / s))
-print("Area equal to: ", ar)
-
-
-# task 4
-
-base = int(input('Enter base: '))
-height = int(input('Enter height:'))
-print('Area equal to: ', base * height)
+parse_interface_status()
