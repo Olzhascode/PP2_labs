d4ec47cf6f050911bd428273fccca60107690828
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-03-12 21:11:57 +0500

    Added volume control

--- lab7/music_player.py
+++ lab7/music_player.py
@@ -59,10 +59,40 @@ while run:
 
                 cover = pygame.image.load(cover_images[track])
                 cover = pygame.transform.scale(cover, (300, 300))
+    #Volume control
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        volume+=0.1
+        pygame.mixer.music.set_volume(volume)
+    if keys[pygame.K_DOWN]:
+        volume-=0.1
+        pygame.mixer.music.set_volume(volume)
 
-
-
+    #covers
     display.fill((255, 255, 255))
+    display.blit(cover, (150, 100))
+
+    button_color = (100, 100, 100)
+    
+    
+    pygame.draw.circle(display, button_color, (300, 450), 30)  
+    if playing and not paused:
+        pygame.draw.rect(display, (255, 255, 255), (290, 435, 8, 30))
+        pygame.draw.rect(display, (255, 255, 255), (302, 435, 8, 30)) 
+    else:
+        pygame.draw.polygon(display, (255, 255, 255), [(290, 435), (290, 465), (315, 450)])  
+
+    
+    pygame.draw.polygon(display, button_color, [(370, 435), (370, 465), (395, 450)]) 
+    pygame.draw.rect(display, button_color, (395, 435, 5, 30))  
+
+    
+    pygame.draw.polygon(display, button_color, [(230, 435), (230, 465), (205, 450)])  
+    pygame.draw.rect(display, button_color, (200, 435, 5, 30))  
+
+    
+    pygame.draw.line(display, button_color, (200, 500), (400, 500), 5)  
+    pygame.draw.circle(display, (255, 0, 0), (200 + int(volume * 200), 500), 10)
     
     pygame.display.flip()
 
