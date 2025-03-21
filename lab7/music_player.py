19c9944be1749aa6021179dccddc37bef51236df
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-03-21 19:12:04 +0500

    adding switchштп between songs and volume control

--- lab7/music_player.py
+++ lab7/music_player.py
@@ -2,12 +2,23 @@ import pygame
 
 pygame.init()
 
-width, height = 800, 600
+width, height = 600, 600
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Music player")
 playlist = ['sounds/The Rolling Stones - Paint It, Black.mp3', 'sounds/Kiss - I Was Made For Lovin You.mp3', 'sounds/The Animals - House Of Rising Sun.mp3', 'sounds/White Town - Your Woman.mp3']
 track = 0
 pygame.mixer.music.load(playlist[track])
+volume = 0.5
+
+cover_images = [
+    "images/rolling stones.jpg",
+    "images/kiss.jpg",
+    "images/the animals.jpg",
+    "images/white town.jpg"
+]
+
+cover = pygame.image.load(cover_images[track])
+cover = pygame.transform.scale(cover, (300, 300))
 
 playing = False
 paused = False
@@ -30,10 +41,39 @@ while run:
                     pygame.mixer.music.play()
                     playing = True
                     paused = False
-            
+            if event.key == pygame.K_LEFT:
+                pygame.mixer.music.stop()
+                track = (track + 1) % len(playlist)
+                pygame.mixer.music.load(playlist[track])
+                pygame.mixer.music.play()
+
+                cover = pygame.image.load(cover_images[track])
+                cover = pygame.transform.scale(cover, (300, 300))
+
+            if event.key == pygame.K_RIGHT:
+                pygame.mixer.music.stop()
+                track = (track - 1) % len(playlist)
+                pygame.mixer.music.load(playlist[track])
+                pygame.mixer.music.play()
+
+                cover = pygame.image.load(cover_images[track])
+                cover = pygame.transform.scale(cover, (300, 300))
 
 
-    display.fill((255, 255, 255))
+
+    #volume
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        volume+=0.1
+        pygame.mixer.music.set_volume(volume)
+    if keys[pygame.K_DOWN]:
+        volume-=0.1
+        pygame.mixer.music.set_volume(volume)
+
+
+
+    #display.fill()
+    display.blit(cover, (150, 100))
     pygame.display.flip()
 
 pygame.quit()
