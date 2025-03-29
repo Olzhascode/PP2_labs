e94bc06e16dac5b1256a24ab16d40e1b21a987b8
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-03-29 17:11:45 +0500

    added function to draw circle and square

--- lab8/paint.py
+++ lab8/paint.py
@@ -23,14 +23,18 @@ colors = {"red": RED, "blue": BLUE, "green": GREEN, "erase": WHITE}
 color = "red"
 
 # Modes
-mode_draw = "Draw"
 radius = 10
-mode = mode_draw # defualt mode
 mouse_down = False
 running = True
-
 starting_position = None
+last_position = None
 
+mode_draw = "Draw"
+mode_rect = "Rect"
+mode_circle = "Circle"
+mode = mode_draw # defualt mode
+
+# Main Loop
 while running:
     screen.fill(WHITE)
     screen.blit(canvas, (0, 0))
@@ -50,14 +54,35 @@ while running:
                 color = "erase"
             elif event.key == pygame.K_c:
                 canvas.fill(WHITE)
-        
+            elif event.key == pygame.K_1:
+                mode = mode_draw
+            elif event.key == pygame.K_2:
+                mode = mode_circle
+            elif event.key == pygame.K_3:
+                mode = mode_rect        
         # Mouse
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_down = True
+            starting_position = event.pos
+            last_position = event.pos
         elif event.type == pygame.MOUSEBUTTONUP:
             mouse_down = False
+            if starting_position and mode in [mode_circle, mode_rect]:
+                end_position = event.pos
+                rect = pygame.Rect(
+                    min(starting_position[0], end_position[0]), 
+                    min(starting_position[1], end_position[1]),
+                    abs(end_position[0] - starting_position[0]), 
+                    abs(end_position[1] - starting_position[1])
+                )
+                if mode == mode_rect:
+                    pygame.draw.rect(canvas, colors[color], rect, 2)
+                elif mode == mode_circle:
+                    pygame.draw.ellipse(canvas, colors[color], rect, 2)
         elif event.type == pygame.MOUSEMOTION and mouse_down:
-            pygame.draw.circle(canvas, colors[color], event.pos, radius)
+            if mode == mode_draw:
+                pygame.draw.line(canvas, colors[color],last_position , event.pos, radius * 2)
+            last_position = event.pos
     pygame.display.flip()
 
 pygame.quit()
