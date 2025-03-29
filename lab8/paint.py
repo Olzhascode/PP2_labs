fe6a3b989f1d580954bd4fb058cd93093dc56b95
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-03-29 15:47:56 +0500

    adding colors, erase and modes

--- lab8/paint.py
+++ lab8/paint.py
@@ -11,11 +11,23 @@ pygame.display.set_caption("Pygame Paint")
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
 
 # Canvas
 canvas = pygame.Surface((WIDTH, HEIGHT))
 canvas.fill(WHITE)
 
+colors = {"red": RED, "blue": BLUE, "green": GREEN, "erase": WHITE}
+color = "red"
+
+# Modes
+mode_draw = "Draw"
+mode_erase = "Erase"
+radius = 10
+mode = mode_draw # defualt mode
+mouse_down = False
 running = True
 
 while running:
@@ -25,7 +37,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == event.K_r:
+                color = "red" 
+            elif event.key == event.K_g:
+                color = "green"
+            elif event.key == event.K_b:
+                color = "blue"
+            elif event.key == event.K_e:
+                color = "erase"
+            
     pygame.display.flip()
 
 pygame.quit()
