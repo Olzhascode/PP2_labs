11ed0583cb32704a2438a0efe200c5e30e1fdd71
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-04-10 21:46:38 +0500

    import csv psycopg2 modules, connecting to PostgreSQL

--- lab10/phone_book.py
+++ lab10/phone_book.py
@@ -1,1 +1,11 @@
-#phonebook
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    dbname="phonebook",
+    user="postgres",
+    password="Kesha2412:)",
+    host="localhost",
+    port="5432"
+)
+cur = conn.cursor()
