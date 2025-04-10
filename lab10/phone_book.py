74ca01ac2e4ac8967e21c35e1eb0ee8ace0b2c26
Olzhas <niyazbekov.olzhas06@gmail.com>
2025-04-10 22:45:55 +0500

    added function to insert data from console and CSV file

--- lab10/phone_book.py
+++ lab10/phone_book.py
@@ -9,3 +9,22 @@ conn = psycopg2.connect(
     port="5432"
 )
 cur = conn.cursor()
+
+# Insert data from CSV
+def insert_from_csv(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # skip header
+        for row in reader:
+            cur.execute("INSERT INTO phonebook (first_name, phone_number) VALUES (%s, %s)", (row[0], row[1]))
+    conn.commit()
+    print("Data from CSV inserted")
+
+
+# Insert data from console
+def insert_from_console():
+    name = input("Enter name: ")
+    phone = input("Enter phone number: ")
+    cur.execute("INSERT INTO phonebook (first_name, phone_number) VALUES (%s, %s)", (name, phone))
+    conn.commit()
+    print("Entry inserted.")
